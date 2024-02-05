897d0a1d4d78cae7dc510866649e53a33f0ccf34
Hussain Abidi <hussainabidi2009@gmail.com>
2024-02-05 13:52:32 +0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,139 @@
+use futures_util::{
+    stream::{SplitSink, SplitStream},
+    SinkExt, StreamExt,
+};
+use std::{collections::HashMap, net::SocketAddr, sync::Arc};
+use tokio::{
+    net::{TcpListener, TcpStream},
+    sync::Mutex
+};
+use tokio_tungstenite::{accept_async, tungstenite::Message, WebSocketStream};
+
+type SplitSocket = (
+    SplitSink<WebSocketStream<TcpStream>, Message>,
+    SplitStream<WebSocketStream<TcpStream>>,
+);
+
+async fn socket_connect(
+    stream: TcpStream,
+    addr: SocketAddr,
+    client_manager: Arc<Mutex<ClientManager>>,
+) {
+    match accept_async(stream).await {
+        Ok(mut socket) => {
+            let ip = addr.ip().to_string();
+            let mut locked_client_manager = client_manager.lock_owned().await;
+
+            let connections = locked_client_manager
+                .clients
+                .values()
+                .filter(|client| client.address == ip)
+                .count();
+
+            if connections > 2 {
+                println!("Socket from {} terminated due to too many connections", ip);
+                socket.close(None).await.unwrap();
+                return;
+            }
+
+            let client = locked_client_manager.create_client(socket.split(), ip.clone()); // ok it died WHY I DONT SEE ERRORS
+
+            println!("Socket {} from {} connected", client.index, ip);
+
+            client.talk(&format!("Hello {}!", client.index)).await;
+            //tokio::spawn(async move {
+            //client.hear().await;
+            //});
+        }
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let client_manager = Arc::new(Mutex::new(ClientManager::new()));
+
+    let listener = TcpListener::bind("localhost:3000").await.unwrap();
+
+    loop {
+        match listener.accept().await {
+            Ok((stream, addr)) => {
+                let client_manager_cloned = client_manager.clone();
+                tokio::spawn(async move {
+                    socket_connect(stream, addr, client_manager_cloned).await;
+                });
+            }
+            Err(e) => println!("Error: {}", e),
+        }
+    }
+}
+
+struct Client {
+    index: usize,
+    address: String,
+    socket: SplitSocket,
+}
+
+impl Client {
+    fn new(index: usize, socket: SplitSocket, address: String) -> Client {
+        Client {
+            index,
+            socket,
+            address,
+        }
+    }
+
+    async fn talk(&mut self, message: &str) {
+        if let Err(e) = self.socket.0.send(Message::text(message)).await {
+            eprintln!("Error sending message: {}", e);
+        }
+    }
+
+    async fn hear(&mut self) {
+        loop {
+            if let Some(Ok(message)) = self.socket.1.next().await {
+                println!("{} from {}: {:?}", self.index, self.address, message);
+            }
+        }
+    }
+
+    fn something(&'static mut self) {
+        tokio::spawn(async move {
+            self.hear().await;
+        });
+    }
+}
+
+struct ClientManager {
+    counter: usize,
+    clients: HashMap<usize, Client>,
+}
+
+impl ClientManager {
+    fn new() -> ClientManager {
+        ClientManager {
+            counter: 0,
+            clients: HashMap::new(),
+        }
+    }
+
+    fn create_client(&mut self, socket: SplitSocket, address: String) -> &mut Client {
+        let index = self.counter;
+        self.counter += 1;
+        self.clients
+            .entry(index)
+            .or_insert_with(|| Client::new(index, socket, address))
+    }
+
+    // SHUT UP
+
+    #[allow(dead_code)]
+    fn destroy_client(&mut self, index: usize) {
+        self.clients.remove(&index);
+    }
+
+    #[allow(dead_code)]
+    fn get_client(&mut self, index: usize) -> Option<&mut Client> {
+        self.clients.get_mut(&index)
+    }
+}
